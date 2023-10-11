9c741a4e2ceb1e2ef691c466f0430591de5aa5a0
ranvirkumarsharma <ranvir.sharma088@gmail.com>
2023-10-12 04:33:55 +0530

    add assert and get attribute

--- ui control/wait test.py
+++ ui control/wait test.py
@@ -3,8 +3,9 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome import service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import select
+from selenium.webdriver.support import select, expected_conditions
 from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.wait import WebDriverWait
 
 driver = webdriver.Chrome()
 driver.implicitly_wait(9)
@@ -24,5 +25,8 @@ driver.find_element(By.XPATH,"//button[normalize-space()='PROCEED TO CHECKOUT']"
 #time.sleep(5)
 driver.find_element(By.XPATH,"//input[@placeholder='Enter promo code']").send_keys("ranvir")
 driver.find_element(By.XPATH,"//button[@class='promoBtn']").click()
-#time.sleep(6)
+#time.sleep(6) time sleep we use when for specific step ew need some time
+#explcitwaittime use for when on specific condition we need some extra time to wait after apply impecit wait on the page to perfrm or execute some step.
+wait = WebDriverWait(driver,10)
+wait.until(expected_conditions.presence_of_all_elements_located((By.CSS_SELECTOR,".promoinfo")))
 print(driver.find_element(By.CSS_SELECTOR,".promoinfo").text);
