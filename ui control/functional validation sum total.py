55578ea118d83105ef82109b8436b622c20e27ee
ranvirkumarsharma <ranvir.sharma088@gmail.com>
2023-10-14 05:45:48 +0530

    add assert and get attribute

--- ui control/functional validation sum total.py
+++ ui control/functional validation sum total.py
@@ -1,5 +1,6 @@
 import time
 
+import total
 from selenium import webdriver
 from selenium.webdriver.chrome import service
 from selenium.webdriver.common.by import By
@@ -22,13 +23,26 @@ for result in results:
 #time.sleep(4)
 driver.find_element(By.XPATH,"//img[@alt='Cart']").click()
 driver.find_element(By.XPATH,"//button[normalize-space()='PROCEED TO CHECKOUT']").click()
+
+#sum validation incart
+prices = driver.find_elements(By.CSS_SELECTOR,"tr td:nth-child(5) p") #somemore option css- td:nth-child(5) p xpath //tr/td[5]/p also //td[5]/p
+sum = 0
+for price in prices:
+    sum = sum + int(price.text)
+#print(sum)
+totalamount = int(driver.find_element(By.CSS_SELECTOR,".totAmt").text)
+assert totalamount == sum
 #time.sleep(5)
-driver.find_element(By.XPATH,"//input[@placeholder='Enter promo code']").send_keys("ranvir")
+driver.find_element(By.XPATH,"//input[@placeholder='Enter promo code']").send_keys("rahulshettyacademy")
 driver.find_element(By.XPATH,"//button[@class='promoBtn']").click()
+afterdiscount = int(driver.find_element(By.CSS_SELECTOR,".discountAmt").text)
+assert afterdiscount <= totalamount
+
 #time.sleep(6) time sleep we use when for specific step ew need some time
 #explcitwaittime use for when on specific condition we need some extra time to wait after apply impecit wait on the page to perfrm or execute some step.
 wait = WebDriverWait(driver,10)
 wait.until(expected_conditions.presence_of_all_elements_located((By.CSS_SELECTOR,".promoinfo")))
 print(driver.find_element(By.CSS_SELECTOR,".promoinfo").text);
+
 
 
