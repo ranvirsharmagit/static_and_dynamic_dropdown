373471908ae6e6cd0ef205f495715fd02deb7114
ranvirkumarsharma <ranvir.sharma088@gmail.com>
2023-10-16 03:59:27 +0530

    add assert and get attribute

--- ui control/functional validation sum total.py
+++ ui control/functional validation sum total.py
@@ -7,6 +7,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import select, expected_conditions
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.wait import WebDriverWait
+#list itemcheck
+expectedList = ['Cucumber - 1 Kg', 'Raspberry - 1/4 Kg', 'Strawberry - 1/4 Kg']
+actualList = []
 
 driver = webdriver.Chrome()
 driver.implicitly_wait(9)
@@ -18,8 +21,9 @@ results = driver.find_elements(By.XPATH,"//div[@class='products']/div")
 count = len(results)
 assert count > 0
 for result in results:
+    actualList.append(result.find_element(By.XPATH,"h4").text)
     result.find_element(By.XPATH,"div/button").click()
-
+assert expectedList == actualList
 #time.sleep(4)
 driver.find_element(By.XPATH,"//img[@alt='Cart']").click()
 driver.find_element(By.XPATH,"//button[normalize-space()='PROCEED TO CHECKOUT']").click()
@@ -30,19 +34,22 @@ sum = 0
 for price in prices:
     sum = sum + int(price.text)
 #print(sum)
-totalamount = int(driver.find_element(By.CSS_SELECTOR,".totAmt").text)
-assert totalamount == sum
+#totalamount = int(driver.find_element(By.CSS_SELECTOR,".totAmt").text)
+#assert totalamount == sum
 #time.sleep(5)
 driver.find_element(By.XPATH,"//input[@placeholder='Enter promo code']").send_keys("rahulshettyacademy")
 driver.find_element(By.XPATH,"//button[@class='promoBtn']").click()
-afterdiscount = int(driver.find_element(By.CSS_SELECTOR,".discountAmt").text)
-assert afterdiscount <= totalamount
+totalamount = int(driver.find_element(By.CSS_SELECTOR,".totAmt").text)
+time.sleep(5)
+afterdiscount = float(driver.find_element(By.CSS_SELECTOR,".discountAmt").text)
+print(afterdiscount)
+assert totalamount > afterdiscount
 
 #time.sleep(6) time sleep we use when for specific step ew need some time
 #explcitwaittime use for when on specific condition we need some extra time to wait after apply impecit wait on the page to perfrm or execute some step.
-wait = WebDriverWait(driver,10)
-wait.until(expected_conditions.presence_of_all_elements_located((By.CSS_SELECTOR,".promoinfo")))
-print(driver.find_element(By.CSS_SELECTOR,".promoinfo").text);
+#wait = WebDriverWait(driver,10)
+#wait.until(expected_conditions.presence_of_all_elements_located((By.CSS_SELECTOR,".promoinfo")))
+#print(driver.find_element(By.CSS_SELECTOR,".promoinfo").text);
 
 
 
