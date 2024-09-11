913d374fdda7b4475805e6c9efde347de6c70034
ranvirsharmagit <ranvir.sharma088@gmail.com>
2024-09-12 01:35:08 +0530

    add comments

--- endtoend testing.py
+++ endtoend testing.py
@@ -20,12 +20,13 @@ driver.find_element(By.CSS_SELECTOR,"a[class*='btn-primary']").click()  #if we u
 time.sleep(4)
 driver.find_element(By.CSS_SELECTOR,"button[class*='btn-success']").click()
 driver.find_element(By.XPATH,"//input[@id='country']").send_keys("Ind")
-wait = WebDriverWait(driver,10)
-wait.until(expected_conditions.presence_of_all_elements_located((By.LINK_TEXT,"India")))
+wait = WebDriverWait(driver,10)    #to give time to load country name list
+wait.until(expected_conditions.presence_of_all_elements_located((By.LINK_TEXT,"India")))   #wait till meet this condition or India appear in list
 driver.find_element(By.LINK_TEXT,"India").click()
-driver.find_element(By.CSS_SELECTOR,"div[class*=checkbox]").click()
+driver.find_element(By.CSS_SELECTOR,"div[class*=checkbox]").click()     #use of * in CSS when you want to choose partial class name
 driver.find_element(By.XPATH,"//input[@type='submit']").click()
 Sucsses = driver.find_element(By.CSS_SELECTOR,"div[class*=alert]").text
 
-assert "Success! Thank you!" in Sucsses
+assert "Success! Thank you!" in Sucsses   # use "in" instead of == in assertion because given keyword must appear in list if we select == then exact keyword should appear
+
 print(Sucsses)
